63299253989908255c4b83af14726ebbc0dc3d7a
patmood <patrick.n.moody@gmail.com>
2013-09-03 02:32:43 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hugegif::Application.config.secret_key_base = '00ea32a3e7bda46fabb649447761f09f7d4100568a24526228e3a0e86f327cbcac0869232c0b9eb0a33b08c76b7a608410cdb66ba40d085c7b531d12d122b23e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hugegif::Application.config.session_store :cookie_store, key: '_hugegif_session'
