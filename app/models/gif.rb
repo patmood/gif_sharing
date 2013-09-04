634b7b3832431fab7fb85e2b9e4cedfcead6915f
patmood <patrick.n.moody@gmail.com>
2013-09-04 01:38:17 -0700

    readded gif model

---
+++ app/models/gif.rb
@@ -0,0 +1,13 @@
+class Gif < ActiveRecord::Base
+
+  def self.unique_random(seen_array = [])
+    gif = uncached { Gif.order("RANDOM()").first }
+    i = 0
+    while seen_array.include?(gif.token) && i < 10
+      gif = uncached { Gif.order("RANDOM()").first }
+      i += 1
+    end
+    gif
+  end
+
+end
